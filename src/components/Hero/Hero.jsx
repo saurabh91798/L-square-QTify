c68e1ea25038b90ac859a8461f03ac8e9ac550bc
K.Saurabh Rao <saurabh91798@gmail.com>
2024-07-18 14:54:33 +0530

    made changes in Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,10 +5,16 @@ const Hero = () => {
   return (
     <div className={styles.hero}>
       <div>
-        <h1> 100 Thousand Songs, ad - free </h1>
-        <h1> Over thousands podcast episodes</h1>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
       </div>
-      <img src={heroImage} alt="Hero_Image" />
+      <div>
+        <img
+          src={heroImage}
+          width={212}
+          alt="headphones"
+        />
+      </div>
     </div>
   );
 };
